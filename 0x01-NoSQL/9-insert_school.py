6ce13cfa04d37d05a71673636c13c664372bec01
betbaba <127618635+betbaba@users.noreply.github.com>
2024-07-18 03:08:25 +0200

    Update 9-insert_school.py
    done

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-"""A Python script that inserts a new document
-   in a collection based on kwargs
-"""
-import pymongo
+'''Task 9's module.
+'''
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection based on kwargs """
-    newId = []
-    for coll in kwargs.values():
-        cursor = mongo_collection.insert_one(coll)
-        newId.append(cursor.inserted_id)
-    return newId
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
